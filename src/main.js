ec7950957bf7c1a6deb088ba7da421d463d847c4
imakandiboi <imakandiboi@yahoo.com>
2022-11-04 20:32:25 -0400

    update main page home

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import { createAuth0 } from '@auth0/auth0-vue';
 import bootstrap from 'bootstrap';
 import router from './router';
 import { domain, client_id } from './auth_config.json';
-
+// GSAP import
+import gsap from "gsap";
+// GSAP other plugins:
+import Draggable from "gsap/Draggable";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import MotionPathPlugin from "gsap/MotionPathPlugin";
+gsap.registerPlugin(ScrollTrigger, Draggable, MotionPathPlugin);
 
 import 'bootstrap'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
